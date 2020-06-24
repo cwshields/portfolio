613e9af291222c59bc3b50b44b1047a475beb0b7
cwshields <cwshields2@gmail.com>
2020-06-23 21:23:54 -0500

    added project page

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import Projects from './components/projects/Projects';
 import Welcome from "./components/welcome/Welcome";
 import Info from "./components/info/Info";
 
@@ -7,5 +8,6 @@ export default (
   <Switch>
     <Route exact path='/' component={Welcome} />
     <Route path='/info' component={Info} />
+    <Route path='/projects' component={Projects} />
   </Switch>
 );
