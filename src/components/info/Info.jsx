dd37c80bc1cf940738e2fe7a14e6f02c3db68362
cwshields <cwshields2@gmail.com>
2020-07-07 21:33:36 -0500

    optomizing practices

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -60,7 +60,7 @@ class Info extends Component {
                         0 .992-.183 1.379-.552.8-.762.831-2.028.069-2.828l-16.619-17.448c-.756-.755-1.76-1.172-2.829-1.172s-2.073.417-2.862 
                         1.207l-16.586 17.414c-.762.8-.731 2.066.069 2.828s2.067.731 2.828-.069l14.551-15.342v39.962c0 1.104.896 2 2 2z"
               />
-              <div id="back-button" className="display-none">Linkedin Link</div>
+              <div id="back-button" className="display-none">Back Button</div>
             </svg>
           </Link>
           <div className="info-wrap">
