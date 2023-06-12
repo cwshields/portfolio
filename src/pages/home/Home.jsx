4d19102fd1c420cdb1dfec542e634603a28ddcb2
cwshields <cwshields2@gmail.com>
2023-06-11 21:55:30 -0500

    Add Fueland experience  - Abstract logos to components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,6 +6,7 @@ import "../../styles/Projects.scss";
 import afp from "../../images/AFPShowcase.jpg";
 import bloglab from "../../images/BlogLab.jpg";
 import fuelrewards from "../../images/FuelRewards.jpg";
+import fueland from "../../images/FuelandInc.jpg";
 import devmountain from "../../images/DevMountain.jpg";
 import drino from "../../images/DrinoShowcase.jpg";
 import meImg from "../../images/me.jpg";
@@ -23,6 +24,9 @@ import css from "../../images/logos/css-logo.png";
 import react from "../../images/logos/react-logo.png";
 import typescript from "../../images/logos/typescript_logo.png";
 import rbs from "../../images/logos/rbs-logo.png";
+import LinkedIn from "../../components/SocialMedia/LinkedIn";
+import Github from "../../components/SocialMedia/Github";
+import Twitter from "../../components/SocialMedia/Twitter";
 
 const imagesPath = {
   meImg: meImg,
@@ -80,17 +84,7 @@ const Home = () => {
                       rel="noopener noreferrer"
                       href="https://www.linkedin.com/in/chase-shields-236bb4126/"
                     >
-                      <svg
-                        className="media-svg"
-                        viewBox="0 0 512 512"
-                        xmlns="http://www.w3.org/2000/svg"
-                        aria-labelledby="linkedin-link"
-                      >
-                        <path
-                          d="m437 0h-362c-41.351562 0-75 33.648438-75 75v362c0 41.351562 33.648438 75 75 
-                            75h362c41.351562 0 75-33.648438 75-75v-362c0-41.351562-33.648438-75-75-75zm-256 406h-60v-210h60zm0-240h-60v-60h60zm210 240h-60v-120c0-16.539062-13.460938-30-30-30s-30 13.460938-30 30v120h-60v-210h60v11.308594c15.71875-4.886719 25.929688-11.308594 45-11.308594 40.691406.042969 75 36.546875 75 79.6875zm0 0"
-                        />
-                      </svg>
+                      <LinkedIn />
                       <div id="linkedin-link" className="display-none">
                         Linkedin Link
                       </div>
@@ -102,17 +96,7 @@ const Home = () => {
                       rel="noopener noreferrer"
                       href="https://github.com/cwshields/"
                     >
-                      <svg
-                        className="media-svg"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 512 512"
-                        aria-labelledby="github-link"
-                      >
-                        <path
-                          d="M255.968,5.329C114.624,5.329,0,120.401,0,262.353c0,113.536,73.344,209.856,175.104,243.872
-                            c12.8,2.368,17.472-5.568,17.472-12.384c0-6.112-0.224-22.272-0.352-43.712c-71.2,15.52-86.24-34.464-86.24-34.464 c-11.616-29.696-28.416-37.6-28.416-37.6c-23.264-15.936,1.728-15.616,1.728-15.616c25.696,1.824,39.2,26.496,39.2,26.496 c22.848,39.264,59.936,27.936,74.528,21.344c2.304-16.608,8.928-27.936,16.256-34.368 c-56.832-6.496-116.608-28.544-116.608-127.008c0-28.064,9.984-51.008,26.368-68.992c-2.656-6.496-11.424-32.64,2.496-68 c0,0,21.504-6.912,70.4,26.336c20.416-5.696,42.304-8.544,64.096-8.64c21.728,0.128,43.648,2.944,64.096,8.672 c48.864-33.248,70.336-26.336,70.336-26.336c13.952,35.392,5.184,61.504,2.56,68c16.416,17.984,26.304,40.928,26.304,68.992 c0,98.72-59.84,120.448-116.864,126.816c9.184,7.936,17.376,23.616,17.376,47.584c0,34.368-0.32,62.08-0.32,70.496 c0,6.88,4.608,14.88,17.6,12.352C438.72,472.145,512,375.857,512,262.353C512,120.401,397.376,5.329,255.968,5.329z"
-                        />
-                      </svg>
+                      <Github />
                       <div id="github-link" className="display-none">
                         Github Link
                       </div>
@@ -124,19 +108,7 @@ const Home = () => {
                       rel="noopener noreferrer"
                       href="https://twitter.com/ChaseShields6"
                     >
-                      <svg
-                        className="media-svg"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 512 512"
-                        aria-labelledby="twitter-link"
-                      >
-                        <path
-                          d="M512,97.248c-19.04,8.352-39.328,13.888-60.48,16.576c21.76-12.992,38.368-33.408,46.176-58.016 
-                          c-20.288,12.096-42.688,20.64-66.56,25.408C411.872,60.704,384.416,48,354.464,48c-58.112,0-104.896,47.168-104.896,104.992 
-                          c0,8.32,0.704,16.32,2.432,23.936c-87.264-4.256-164.48-46.08-216.352-109.792c-9.056,15.712-14.368,33.696-14.368,53.056	c0,36.352,18.72,68.576,46.624,87.232c-16.864-0.32-33.408-5.216-47.424-12.928c0,0.32,0,0.736,0,1.152	
-                          c0,51.008,36.384,93.376,84.096,103.136c-8.544,2.336-17.856,3.456-27.52,3.456c-6.72,0-13.504-0.384-19.872-1.792 c13.6,41.568,52.192,72.128,98.08,73.12c-35.712,27.936-81.056,44.768-130.144,44.768c-8.608,0-16.864-0.384-25.12-1.44 C46.496,446.88,101.6,464,161.024,464c193.152,0,298.752-160,298.752-298.688c0-4.64-0.16-9.12-0.384-13.568 C480.224,136.96,497.728,118.496,512,97.248z"
-                        />
-                      </svg>
+                      <Twitter />
                       <div id="twitter-link" className="display-none">
                         Twitter Link
                       </div>
@@ -199,6 +171,36 @@ const Home = () => {
         <div className="projects-wrap">
           <div className="showcase-wrap">
             <div className="showcase">
+              <img
+                className="proj-img"
+                src={fueland}
+                alt="drino-showcase.png"
+              />
+            </div>
+            <div className="text">
+              <div className="title-wrap">
+                <h1>Fueland Inc</h1>
+              </div>
+              <p className="desc">
+                At Fueland Inc I developed the landing page and signup form for
+                businesses to register their company and stores with the
+                Fueland's Fuel Reward program.
+              </p>
+              <p>It involved a number of technologies such as:</p>
+              <ul>
+                <li>React/Typescript</li>
+                <li>Login/Registration functionality</li>
+                <li>Responsive form selections</li>
+                <li>Password resetting</li>
+                <li>Signeasy form signing</li>
+                <li>Java backend</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="projects-wrap">
+          <div className="showcase-wrap">
+            <div className="showcase">
               <a
                 href="https://www.fuelrewards.com/"
                 target="_blank"
@@ -222,15 +224,15 @@ const Home = () => {
                 CSS, JavaScript, Nunjucks, SASS, JSON, jQuery and Gulp building
                 the login, registration and success pages for sponsors of Shell
                 like Bank of America, Reddit, Sonesta, Red Bull, Dunkin Donuts,
-                CarAdvise, American Airlines, and many more. This project
-                includes several technologies such as:
+                CarAdvise, American Airlines, and many more.
               </p>
+              <p>Features Include:</p>
               <ul>
                 <li>Registration processes</li>
                 <li>Login functionality</li>
                 <li>CMS Utilization</li>
-                <li>Reactive form selections</li>
-                <li>Password resetting service</li>
+                <li>Responsive form selections</li>
+                <li>Password resetting</li>
                 <li>Gulp workflows</li>
               </ul>
             </div>
@@ -269,11 +271,18 @@ const Home = () => {
           <div className="projects-wrap">
             <div className="showcase-wrap">
               <div className="showcase">
-                <img
-                  className="proj-img"
-                  src={bloglab}
-                  alt="drino-showcase.png"
-                />
+                <a
+                  href="https://bloglab.live/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  alt="BlogLab"
+                >
+                  <img
+                    className="proj-img"
+                    src={bloglab}
+                    alt="drino-showcase.png"
+                  />
+                </a>
               </div>
               <div className="text">
                 <div className="title-wrap">
@@ -285,17 +294,7 @@ const Home = () => {
                     rel="noopener noreferrer"
                     href="https://github.com/cwshields/bloglab-web"
                   >
-                    <svg
-                      className="media-svg"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 512 512"
-                      aria-labelledby="github-link"
-                    >
-                      <path
-                        d="M255.968,5.329C114.624,5.329,0,120.401,0,262.353c0,113.536,73.344,209.856,175.104,243.872
-                      c12.8,2.368,17.472-5.568,17.472-12.384c0-6.112-0.224-22.272-0.352-43.712c-71.2,15.52-86.24-34.464-86.24-34.464 c-11.616-29.696-28.416-37.6-28.416-37.6c-23.264-15.936,1.728-15.616,1.728-15.616c25.696,1.824,39.2,26.496,39.2,26.496 c22.848,39.264,59.936,27.936,74.528,21.344c2.304-16.608,8.928-27.936,16.256-34.368 c-56.832-6.496-116.608-28.544-116.608-127.008c0-28.064,9.984-51.008,26.368-68.992c-2.656-6.496-11.424-32.64,2.496-68 c0,0,21.504-6.912,70.4,26.336c20.416-5.696,42.304-8.544,64.096-8.64c21.728,0.128,43.648,2.944,64.096,8.672 c48.864-33.248,70.336-26.336,70.336-26.336c13.952,35.392,5.184,61.504,2.56,68c16.416,17.984,26.304,40.928,26.304,68.992 c0,98.72-59.84,120.448-116.864,126.816c9.184,7.936,17.376,23.616,17.376,47.584c0,34.368-0.32,62.08-0.32,70.496 c0,6.88,4.608,14.88,17.6,12.352C438.72,472.145,512,375.857,512,262.353C512,120.401,397.376,5.329,255.968,5.329z"
-                      />
-                    </svg>
+                    <Github />
                     <div id="github-link" className="display-none">
                       Github Link
                     </div>
@@ -304,12 +303,13 @@ const Home = () => {
                 <p className="desc">
                   BlogLab is my current personal project that I'm building where
                   a user will be able to post, edit and share blogs, listings,
-                  podcasts, guides and more. This project includes:
+                  podcasts, guides and more.
                 </p>
                 Current features:
                 <ul>
                   <li>Data driven home page</li>
                   <li>Advanced routing</li>
+                  <li>Custom Logo Design</li>
                   <li>Tag constructors</li>
                 </ul>
                 Coming soon:
@@ -341,17 +341,7 @@ const Home = () => {
                     rel="noopener noreferrer"
                     href="https://github.com/cwshields/drino-app"
                   >
-                    <svg
-                      className="media-svg"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 512 512"
-                      aria-labelledby="github-link"
-                    >
-                      <path
-                        d="M255.968,5.329C114.624,5.329,0,120.401,0,262.353c0,113.536,73.344,209.856,175.104,243.872
-                      c12.8,2.368,17.472-5.568,17.472-12.384c0-6.112-0.224-22.272-0.352-43.712c-71.2,15.52-86.24-34.464-86.24-34.464 c-11.616-29.696-28.416-37.6-28.416-37.6c-23.264-15.936,1.728-15.616,1.728-15.616c25.696,1.824,39.2,26.496,39.2,26.496 c22.848,39.264,59.936,27.936,74.528,21.344c2.304-16.608,8.928-27.936,16.256-34.368 c-56.832-6.496-116.608-28.544-116.608-127.008c0-28.064,9.984-51.008,26.368-68.992c-2.656-6.496-11.424-32.64,2.496-68 c0,0,21.504-6.912,70.4,26.336c20.416-5.696,42.304-8.544,64.096-8.64c21.728,0.128,43.648,2.944,64.096,8.672 c48.864-33.248,70.336-26.336,70.336-26.336c13.952,35.392,5.184,61.504,2.56,68c16.416,17.984,26.304,40.928,26.304,68.992 c0,98.72-59.84,120.448-116.864,126.816c9.184,7.936,17.376,23.616,17.376,47.584c0,34.368-0.32,62.08-0.32,70.496 c0,6.88,4.608,14.88,17.6,12.352C438.72,472.145,512,375.857,512,262.353C512,120.401,397.376,5.329,255.968,5.329z"
-                      />
-                    </svg>
+                    <Github />
                     <div id="github-link" className="display-none">
                       Github Link
                     </div>
@@ -388,21 +378,7 @@ const Home = () => {
                     rel="noopener noreferrer"
                     href="https://github.com/cwshields/AFP"
                   >
-                    <svg
-                      className="media-svg"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 512 512"
-                      aria-labelledby="github-link"
-                    >
-                      <path
-                        d="M255.968,5.329C114.624,5.329,0,120.401,0,262.353c0,113.536,73.344,209.856,175.104,243.872
-                      c12.8,2.368,17.472-5.568,17.472-12.384c0-6.112-0.224-22.272-0.352-43.712c-71.2,15.52-86.24-34.464-86.24-34.464 
-                      c-11.616-29.696-28.416-37.6-28.416-37.6c-23.264-15.936,1.728-15.616,1.728-15.616c25.696,1.824,39.2,26.496,39.2,26.496 
-                      c22.848,39.264,59.936,27.936,74.528,21.344c2.304-16.608,8.928-27.936,16.256-34.368 
-                      c-56.832-6.496-116.608-28.544-116.608-127.008c0-28.064,9.984-51.008,26.368-68.992c-2.656-6.496-11.424-32.64,2.496-68 
-                      c0,0,21.504-6.912,70.4,26.336c20.416-5.696,42.304-8.544,64.096-8.64c21.728,0.128,43.648,2.944,64.096,8.672 c48.864-33.248,70.336-26.336,70.336-26.336c13.952,35.392,5.184,61.504,2.56,68c16.416,17.984,26.304,40.928,26.304,68.992 c0,98.72-59.84,120.448-116.864,126.816c9.184,7.936,17.376,23.616,17.376,47.584c0,34.368-0.32,62.08-0.32,70.496 c0,6.88,4.608,14.88,17.6,12.352C438.72,472.145,512,375.857,512,262.353C512,120.401,397.376,5.329,255.968,5.329z"
-                      />
-                    </svg>
+                    <Github />
                     <div id="github-link" className="display-none">
                       Github Link
                     </div>
