6b2b6977e0d2e26e5e6827cd9d8eb54261503219
cwshields <cwshields2@gmail.com>
2026-07-13 16:48:49 -0500

    Optimize file compression
    - Convert files to standard web formats

--- src/pages/welcome/Welcome.tsx
+++ src/pages/welcome/Welcome.tsx
@@ -3,26 +3,26 @@ import { Link } from "react-router-dom";
 import "../../styles/Welcome.scss";
 
 // Logos
-import cslogo from "../../images/logos/cs-logo-light.png";
-import sass from "../../images/logos/sass-logo.png";
-import html from "../../images/logos/html-logo.png";
-import btstrap from "../../images/logos/bootstrap-logo.png";
-import figma from "../../images/logos/figma-logo.png";
-import rbs from "../../images/logos/rbs-logo.png";
-import vue from "../../images/logos/vue-logo.png";
-import ps from "../../images/logos/ps-logo.png";
-import postman from "../../images/logos/postman-logo.png";
-import java from "../../images/logos/java-logo.png";
-import github from "../../images/logos/github-logo.png";
-import nodejs from "../../images/logos/nodejs-logo.png";
-import git from "../../images/logos/git-logo.png";
-import js from "../../images/logos/js-logo.png";
-import ai from "../../images/logos/ai-logo.png";
-import sql from "../../images/logos/mysql-logo.png";
-import css from "../../images/logos/css-logo.png";
-import react from "../../images/logos/react-logo.png";
-import flutter from "../../images/logos/flutter-logo.png";
-import dart from "../../images/logos/dart-logo.png";
+import cslogo from "../../images/logos/cs-logo-light.webp";
+import sass from "../../images/logos/sass-logo.webp";
+import html from "../../images/logos/html-logo.webp";
+import btstrap from "../../images/logos/bootstrap-logo.webp";
+import figma from "../../images/logos/figma-logo.webp";
+import typescript from "../../images/logos/typescript_logo.webp";
+import vue from "../../images/logos/vue-logo.webp";
+import ps from "../../images/logos/ps-logo.webp";
+import postman from "../../images/logos/postman-logo.webp";
+import java from "../../images/logos/java-logo.webp";
+import github from "../../images/logos/github-logo.webp";
+import nodejs from "../../images/logos/nodejs-logo.webp";
+import git from "../../images/logos/git-logo.webp";
+import js from "../../images/logos/js-logo.webp";
+import ai from "../../images/logos/ai-logo.webp";
+import sql from "../../images/logos/mysql-logo.webp";
+import css from "../../images/logos/css-logo.webp";
+import react from "../../images/logos/react-logo.webp";
+import flutter from "../../images/logos/flutter-logo.webp";
+import dart from "../../images/logos/dart-logo.webp";
 
 function Welcome() {
   return (
@@ -56,7 +56,7 @@ function Welcome() {
           <img className="ai logo" src={ai} alt="ai-logo.png" />
           <img className="sql logo" src={sql} alt="sql-logo.png" />
           <img className="css logo" src={css} alt="css-logo.png" />
-          <img className="rbs logo" src={rbs} alt="rbs-logo.png" />
+          <img className="rbs logo" src={typescript} alt="rbs-logo.png" />
           <img className="flutter logo" src={flutter} alt="flutter-logo.png" />
           <img className="dart logo" src={dart} alt="dart-logo.png" />
         </div>
