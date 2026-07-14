875a10fcd42a1343d06ff3776e86c989d72255a2
cwshields <cwshields2@gmail.com>
2026-07-13 23:07:55 -0500

    Fix resposive layouts

--- src/pages/ScrabbleGame/ScrabbleGame.tsx
+++ src/pages/ScrabbleGame/ScrabbleGame.tsx
@@ -91,139 +91,145 @@ function ScrabbleGame() {
 
   return (
     <div className="scrabble-game">
-      {showNewGameModal && (
-        <NewGameModal
-          onStart={newGame.startNewGame}
-          onCancel={newGame.closeNewGameModal}
-          canCancel={canCancelNewGameModal}
-        />
-      )}
-      {debugMode && (
-        <DebugPanel
-          p1inv={p1inv}
-          p2inv={p2inv}
-          bag={bag}
-          gameMode={gameMode}
-          botDifficulty={botDifficulty}
-          p1Label={playerLabel(true)}
-          p2Label={playerLabel(false)}
-          debugRevealBotRack={debugRevealBotRack}
-          debugAllowMoveLocked={debugAllowMoveLocked}
-          debugBotMoveLog={debugBotMoveLog}
-          onClose={debugTools.closeDebugPanel}
-          onReshuffleRack={debugTools.debugReshuffleRack}
-          onResetHints={debugTools.debugResetHints}
-          onToggleRevealBotRack={debugTools.debugToggleRevealBotRack}
-          onToggleAllowMoveLocked={debugTools.debugToggleAllowMoveLocked}
-          onForceStalemateCheck={debugTools.debugForceStalemateCheck}
-          onSetRack={debugTools.debugSetRack}
-        />
-      )}
-      <TopActionBar
-        onNewGame={newGame.openNewGameModal}
-        onSaveToFile={handleSaveToFile}
-        onLoadFileClick={handleLoadFileClick}
-        onFileSelected={handleFileSelected}
-        fileInputRef={fileInputRef}
-        loadDisabled={validating || botThinking}
-        onToggleHistory={toggleHistory}
-      />
-      <HistoryPanel
-        showHistory={showHistory}
-        moveHistory={moveHistory}
-        playerLabel={playerLabel}
-        onToggleHistory={toggleHistory}
-        onSetHoveredTurn={setHoveredTurn}
-        onClearHoveredTurn={clearHoveredTurn}
-      />
-      <div className="letters-left">Tiles left: {bag.length}</div>
-      <div className="score">Score: {p1score}</div>
-      <div className="rack-row">
-        <Rack
-          letters={p1inv}
-          isActive={p1turn && !gameOver}
-          selectedIndices={selectedIndices}
-          dragSource={dragSource}
-          onToggleSelect={moveActions.toggleLetterSelect}
-          onDragStart={dragAndDrop.handleRackDragStart}
-          onDragEnd={dragAndDrop.handleDragEnd}
-          onDrop={dragAndDrop.handleRackDrop}
-        />
-        {p1turn && (
-          <RackActions
-            pendingCount={pendingCount}
-            hintsUsed={p1hintsUsed}
-            validating={validating}
-            gameOver={gameOver}
-            findingHint={findingHint}
-            onShuffle={moveActions.shuffleRackAction}
-            onClear={moveActions.clearPending}
-            onHint={giveHint}
+      <div className="scrabble-mobile-blocker">
+        <p>Mobile development is still in progress...</p>
+        <p>Try it out on a PC instead!</p>
+      </div>
+      <div className="scrabble-game-content">
+        {showNewGameModal && (
+          <NewGameModal
+            onStart={newGame.startNewGame}
+            onCancel={newGame.closeNewGameModal}
+            canCancel={canCancelNewGameModal}
           />
         )}
-      </div>
-      <div className="tile-wrap">
-        <BoardGrid
-          board={board}
-          dragSource={dragSource}
-          highlightedCellKeys={highlightedCellKeys}
-          pendingWordStatus={moveActions.pendingWordStatus}
-          disabled={validating || gameOver}
-          debugMovable={debugAllowMoveLocked}
-          onDropLetter={dragAndDrop.handleBoardDrop}
-          onDragStartLetter={dragAndDrop.handleBoardDragStart}
-          onDragEnd={dragAndDrop.handleDragEnd}
+        {debugMode && (
+          <DebugPanel
+            p1inv={p1inv}
+            p2inv={p2inv}
+            bag={bag}
+            gameMode={gameMode}
+            botDifficulty={botDifficulty}
+            p1Label={playerLabel(true)}
+            p2Label={playerLabel(false)}
+            debugRevealBotRack={debugRevealBotRack}
+            debugAllowMoveLocked={debugAllowMoveLocked}
+            debugBotMoveLog={debugBotMoveLog}
+            onClose={debugTools.closeDebugPanel}
+            onReshuffleRack={debugTools.debugReshuffleRack}
+            onResetHints={debugTools.debugResetHints}
+            onToggleRevealBotRack={debugTools.debugToggleRevealBotRack}
+            onToggleAllowMoveLocked={debugTools.debugToggleAllowMoveLocked}
+            onForceStalemateCheck={debugTools.debugForceStalemateCheck}
+            onSetRack={debugTools.debugSetRack}
+          />
+        )}
+        <TopActionBar
+          onNewGame={newGame.openNewGameModal}
+          onSaveToFile={handleSaveToFile}
+          onLoadFileClick={handleLoadFileClick}
+          onFileSelected={handleFileSelected}
+          fileInputRef={fileInputRef}
+          loadDisabled={validating || botThinking}
+          onToggleHistory={toggleHistory}
         />
-        <GameSidebar
-          gameOverOffer={gameOverOffer}
-          gameOver={gameOver}
-          p1score={p1score}
-          p2score={p2score}
+        <HistoryPanel
+          showHistory={showHistory}
+          moveHistory={moveHistory}
           playerLabel={playerLabel}
-          botTurnActive={botTurnActive}
-          botThinking={botThinking}
-          botLabel={botLabel}
-          p1turn={p1turn}
-          firstTurn={firstTurn}
-          pendingScore={pendingScore}
-          validationError={validationError}
-          selectedCount={selectedIndices.length}
-          bagLength={bag.length}
-          validating={validating}
-          pendingCount={pendingCount}
-          onEndGame={newGame.endGame}
-          onDismissGameOverOffer={newGame.dismissGameOverOffer}
-          onTradeLetters={moveActions.tradeLetters}
-          onPassTurn={moveActions.passTurn}
-          onSubmit={moveActions.submit}
+          onToggleHistory={toggleHistory}
+          onSetHoveredTurn={setHoveredTurn}
+          onClearHoveredTurn={clearHoveredTurn}
         />
-      </div>
-      <div className="rack-row">
-        <Rack
-          letters={p2inv}
-          isActive={!p1turn && !gameOver}
-          hideLetters={gameMode === "1p" && !debugRevealBotRack}
-          selectedIndices={selectedIndices}
-          dragSource={dragSource}
-          onToggleSelect={moveActions.toggleLetterSelect}
-          onDragStart={dragAndDrop.handleRackDragStart}
-          onDragEnd={dragAndDrop.handleDragEnd}
-          onDrop={dragAndDrop.handleRackDrop}
-        />
-        {!p1turn && gameMode !== "1p" && (
-          <RackActions
-            pendingCount={pendingCount}
-            hintsUsed={p2hintsUsed}
-            validating={validating}
-            gameOver={gameOver}
-            findingHint={findingHint}
-            onShuffle={moveActions.shuffleRackAction}
-            onClear={moveActions.clearPending}
-            onHint={giveHint}
+        <div className="letters-left">Tiles left: {bag.length}</div>
+        <div className="score">Score: {p1score}</div>
+        <div className="rack-row">
+          <Rack
+            letters={p1inv}
+            isActive={p1turn && !gameOver}
+            selectedIndices={selectedIndices}
+            dragSource={dragSource}
+            onToggleSelect={moveActions.toggleLetterSelect}
+            onDragStart={dragAndDrop.handleRackDragStart}
+            onDragEnd={dragAndDrop.handleDragEnd}
+            onDrop={dragAndDrop.handleRackDrop}
           />
-        )}
+          {p1turn && (
+            <RackActions
+              pendingCount={pendingCount}
+              hintsUsed={p1hintsUsed}
+              validating={validating}
+              gameOver={gameOver}
+              findingHint={findingHint}
+              onShuffle={moveActions.shuffleRackAction}
+              onClear={moveActions.clearPending}
+              onHint={giveHint}
+            />
+          )}
+        </div>
+        <div className="tile-wrap">
+          <BoardGrid
+            board={board}
+            dragSource={dragSource}
+            highlightedCellKeys={highlightedCellKeys}
+            pendingWordStatus={moveActions.pendingWordStatus}
+            disabled={validating || gameOver}
+            debugMovable={debugAllowMoveLocked}
+            onDropLetter={dragAndDrop.handleBoardDrop}
+            onDragStartLetter={dragAndDrop.handleBoardDragStart}
+            onDragEnd={dragAndDrop.handleDragEnd}
+          />
+          <GameSidebar
+            gameOverOffer={gameOverOffer}
+            gameOver={gameOver}
+            p1score={p1score}
+            p2score={p2score}
+            playerLabel={playerLabel}
+            botTurnActive={botTurnActive}
+            botThinking={botThinking}
+            botLabel={botLabel}
+            p1turn={p1turn}
+            firstTurn={firstTurn}
+            pendingScore={pendingScore}
+            validationError={validationError}
+            selectedCount={selectedIndices.length}
+            bagLength={bag.length}
+            validating={validating}
+            pendingCount={pendingCount}
+            onEndGame={newGame.endGame}
+            onDismissGameOverOffer={newGame.dismissGameOverOffer}
+            onTradeLetters={moveActions.tradeLetters}
+            onPassTurn={moveActions.passTurn}
+            onSubmit={moveActions.submit}
+          />
+        </div>
+        <div className="rack-row">
+          <Rack
+            letters={p2inv}
+            isActive={!p1turn && !gameOver}
+            hideLetters={gameMode === "1p" && !debugRevealBotRack}
+            selectedIndices={selectedIndices}
+            dragSource={dragSource}
+            onToggleSelect={moveActions.toggleLetterSelect}
+            onDragStart={dragAndDrop.handleRackDragStart}
+            onDragEnd={dragAndDrop.handleDragEnd}
+            onDrop={dragAndDrop.handleRackDrop}
+          />
+          {!p1turn && gameMode !== "1p" && (
+            <RackActions
+              pendingCount={pendingCount}
+              hintsUsed={p2hintsUsed}
+              validating={validating}
+              gameOver={gameOver}
+              findingHint={findingHint}
+              onShuffle={moveActions.shuffleRackAction}
+              onClear={moveActions.clearPending}
+              onHint={giveHint}
+            />
+          )}
+        </div>
+        <div className="score">Score: {p2score}</div>
       </div>
-      <div className="score">Score: {p2score}</div>
     </div>
   );
 }
